175ff0fa09e4c11c07ddb2c288cc0ccd921d295a
Vinay Patidar <vpatidar94@gmail.com>
2023-09-03 23:03:03 +0530

    aaded api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    BASE_API_URL: 'http://localhost:5000',
+    BASE_API_URL: 'https://aayam-star-api-6bqic.ondigitalocean.app',
     QUESTIONS_API_URL: 'https://api.aayamcareerinstitute.co.in/api',
     ONLINE_TEST_ID: 112
 };
